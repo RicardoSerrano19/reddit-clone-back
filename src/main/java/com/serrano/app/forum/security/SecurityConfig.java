c5116f52dc66d0e963f36e7a79a8f6820130b2d4
Ricardo Serrano <ricardo.ss@live.com>
2022-02-15 13:53:29 -0600

    feat: Add configuration for Spring Security

---
+++ src/main/java/com/serrano/app/forum/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.serrano.app.forum.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/api/auth/**", "/h2-console/**")
+			.permitAll();
+		http.headers().frameOptions().disable();
+		http.authorizeRequests().anyRequest().authenticated();
+			
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
