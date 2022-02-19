88d284a1fc4a51f70e190ce0a4e9846ecfc440f5
Ricardo Serrano <ricardo.ss@live.com>
2022-02-18 21:54:59 -0600

    feat: Add relationship with role

--- src/main/java/com/serrano/app/forum/domain/User.java
+++ src/main/java/com/serrano/app/forum/domain/User.java
@@ -1,20 +1,23 @@
 package com.serrano.app.forum.domain;
 
 import java.time.Instant;
+import java.util.Set;
+import java.util.HashSet;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
@@ -26,4 +29,6 @@ public class User {
 	private String email;
 	private Instant created_at;
 	private Boolean enabled;
+	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<Role>();
 }
