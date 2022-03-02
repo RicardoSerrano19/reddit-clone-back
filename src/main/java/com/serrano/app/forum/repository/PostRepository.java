1746b476d95f024b9a42e79e61d62dbb6794b4e9
Ricardo Serrano <ricardo.ss@live.com>
2022-03-02 12:59:04 -0600

    feat: Find by Name and Category

--- src/main/java/com/serrano/app/forum/repository/PostRepository.java
+++ src/main/java/com/serrano/app/forum/repository/PostRepository.java
@@ -1,11 +1,17 @@
 package com.serrano.app.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.serrano.app.forum.domain.Category;
 import com.serrano.app.forum.domain.Post;
+import com.serrano.app.forum.domain.User;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
+	List<Post> findByUser(User user);
+	List<Post> findByCategory(Category category);
 
 }
