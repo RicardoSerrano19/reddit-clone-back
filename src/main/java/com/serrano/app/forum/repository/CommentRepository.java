798c06cf498fcaf13014d26a16229990775bf456
Ricardo Serrano <ricardo.ss@live.com>
2022-03-04 14:51:56 -0600

    feat: Find by Username and Post

--- src/main/java/com/serrano/app/forum/repository/CommentRepository.java
+++ src/main/java/com/serrano/app/forum/repository/CommentRepository.java
@@ -1,11 +1,17 @@
 package com.serrano.app.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.serrano.app.forum.domain.Comment;
+import com.serrano.app.forum.domain.Post;
+import com.serrano.app.forum.domain.User;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>{
+	List<Comment> findByPost(Post post);
 
+	List<Comment> findByUser(User user);
 }
