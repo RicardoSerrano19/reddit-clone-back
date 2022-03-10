489098a308776f1cc1e5be3ae9d25ec83c2c5e0f
Ricardo Serrano <ricardo.ss@live.com>
2022-03-10 12:29:08 -0600

    feat: Add method to findByUserAndPost

--- src/main/java/com/serrano/app/forum/repository/VoteRepository.java
+++ src/main/java/com/serrano/app/forum/repository/VoteRepository.java
@@ -1,11 +1,18 @@
 package com.serrano.app.forum.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.serrano.app.forum.domain.Post;
+import com.serrano.app.forum.domain.User;
 import com.serrano.app.forum.domain.Vote;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long>{
-
+	// Find Vote by Post And User
+	// Order by Desc
+	// Get only one
+	Optional<Vote> findTopByPostAndUserOrderByIdDesc(Post post, User user);
 }
